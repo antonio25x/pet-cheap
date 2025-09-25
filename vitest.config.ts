0330bf2c4255c65e1385ce594a7ec68bc8c794a6
Luis Noria <antonio17x@hotmail.com>
2025-09-25 19:55:48 -0400

    chore: add test for useAuth hook

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ["*/__tests__/**/*.test.ts"],
+    include: ["**/__tests__/**/*.test.{ts,tsx}"],
     setupFiles: ["./vitest.setup.ts"],
+    environment: "jsdom",
   },
 });
